de987857513aa2d12c1b32b1f78351ed4013171a
starlight321 <358683106@qq.com>
2021-08-25 17:44:47 +0800

    add lint-staged

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 let myName = 'Tom';
-console.log(`My name is ${myNane}`);
+console.log(`My name is ${myName}`);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let myName = 'Tom';
+console.log(`My name is ${myNane}`);
+console.log(`My name is ${myName.toStrng()}`);
+interface Foo {}
